192e1684a06c116ef619f41e6f060722a53d95d0
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-22 22:01:28 +0530

    Initialize android project

---
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/news/NewsContract.kt
@@ -0,0 +1,10 @@
+package com.crazyhitty.chdev.ks.news.news
+
+/**
+ * Author:      Kartik Sharma
+ * Email Id:    cr42yh17m4n@gmail.com
+ * Created:     1/22/18 9:57 PM
+ * Description: Unavailable
+ */
+class NewsContract {
+}
