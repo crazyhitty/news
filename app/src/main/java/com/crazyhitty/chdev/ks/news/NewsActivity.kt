009dda0e606ba57feab8fdb1bc0abf980bcfc4fd
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-22 22:45:31 +0530

    Rename MainActivity to NewsActivity

--- app/src/main/java/com/crazyhitty/chdev/ks/news/NewsActivity.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/NewsActivity.kt
@@ -3,10 +3,10 @@ package com.crazyhitty.chdev.ks.news
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class HomeActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_news)
     }
 }
