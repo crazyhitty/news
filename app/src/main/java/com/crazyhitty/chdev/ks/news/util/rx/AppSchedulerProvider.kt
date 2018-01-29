edf1cc3b480b2985d78fec805b9409a3e7272e06
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-01-30 00:45:15 +0530

    Add rx schedulers separately

---
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/util/rx/AppSchedulerProvider.kt
@@ -0,0 +1,20 @@
+package com.crazyhitty.chdev.ks.news.util.rx
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Implementation of [SchedulerProvider].
+ *
+ * @author  Kartik Sharma (cr42yh17m4n@gmail.com)
+ */
+class AppSchedulerProvider : SchedulerProvider {
+    override fun ui(): Scheduler {
+        return AndroidSchedulers.mainThread()
+    }
+
+    override fun io(): Scheduler {
+        return Schedulers.io()
+    }
+}
