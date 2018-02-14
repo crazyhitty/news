f7d229b81dfa7a6c2ed9217223b7cd22c62b9b8f
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-02-15 00:20:07 +0530

    Update styles/UI

--- app/src/main/java/com/crazyhitty/chdev/ks/news/base/BaseAppCompatActivity.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/base/BaseAppCompatActivity.kt
@@ -1,11 +1,15 @@
 package com.crazyhitty.chdev.ks.news.base
 
 import android.content.Context
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.crazyhitty.chdev.ks.news.NewsApplication
+import com.crazyhitty.chdev.ks.news.R
 import com.crazyhitty.chdev.ks.news.di.components.ActivityComponent
 import com.crazyhitty.chdev.ks.news.di.components.DaggerActivityComponent
 import com.crazyhitty.chdev.ks.news.di.modules.ActivityModule
+import org.jetbrains.anko.find
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 /**
@@ -24,5 +28,13 @@ abstract class BaseAppCompatActivity : AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        // Set toolbar if available.
+        val toolbar = find<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
