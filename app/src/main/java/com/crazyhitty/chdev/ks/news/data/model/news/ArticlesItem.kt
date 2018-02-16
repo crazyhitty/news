3990b130bd47e085470ecdd42ff770e9fe8b752e
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-02-16 17:25:54 +0530

    Make some data class parcelable

--- app/src/main/java/com/crazyhitty/chdev/ks/news/data/model/news/ArticlesItem.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/data/model/news/ArticlesItem.kt
@@ -1,5 +1,8 @@
 package com.crazyhitty.chdev.ks.news.data.model.news
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class ArticlesItem(
         val publishedAt: String? = null,
         val author: String? = null,
@@ -7,5 +10,37 @@ data class ArticlesItem(
         val description: String? = null,
         val source: Source? = null,
         val title: String? = null,
-        val url: String? = null
-)
+        val url: String? = null): Parcelable{
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readParcelable(Source::class.java.classLoader),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(publishedAt)
+        parcel.writeString(author)
+        parcel.writeString(urlToImage)
+        parcel.writeString(description)
+        parcel.writeParcelable(source, flags)
+        parcel.writeString(title)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ArticlesItem> {
+        override fun createFromParcel(parcel: Parcel): ArticlesItem {
+            return ArticlesItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ArticlesItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/crazyhitty/chdev/ks/news/data/model/news/Source.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/data/model/news/Source.kt
@@ -1,6 +1,26 @@
 package com.crazyhitty.chdev.ks.news.data.model.news
 
-data class Source(
-	val name: String? = null,
-	val id: Any? = null
-)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Source(val name: String? = null, val id: Any? = null) : Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString(), parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Source> {
+        override fun createFromParcel(parcel: Parcel): Source {
+            return Source(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Source?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
