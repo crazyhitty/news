419c0368fe78910c1a25d04bce8859f950dbad70
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-02-21 18:28:14 +0530

    Add sources and everything api endpoints

--- app/src/main/java/com/crazyhitty/chdev/ks/news/data/api/NewsApiService.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/data/api/NewsApiService.kt
@@ -1,6 +1,7 @@
 package com.crazyhitty.chdev.ks.news.data.api
 
 import com.crazyhitty.chdev.ks.news.data.api.model.news.News
+import com.crazyhitty.chdev.ks.news.data.api.model.news.Source
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,6 +12,14 @@ import retrofit2.http.Query
  * @author  Kartik Sharma (cr42yh17m4n@gmail.com)
  */
 interface NewsApiService {
+    /**
+     * Fetches all of the news sources available.
+     *
+     * @return
+     * [Single] observable containing [Source].
+     */
+    @GET("/v2/sources")
+    fun sources(): Single<Source>
 
     /**
      * Fetches the top headlines.
@@ -21,4 +30,13 @@ interface NewsApiService {
     @GET("/v2/top-headlines")
     fun topHeadlines(@Query("country") country: String,
                      @Query("pageNumber") pageNumber: Int): Single<News>
+
+    /**
+     * Fetches all of the news from a particular source.
+     *
+     * @return
+     * [Single] observable containing [News].
+     */
+    @GET("/v2/everything")
+    fun everything(@Query("source") source: String): Single<News>
 }
--- app/src/main/java/com/crazyhitty/chdev/ks/news/data/api/model/news/Source.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/data/api/model/news/Source.kt
@@ -3,11 +3,30 @@ package com.crazyhitty.chdev.ks.news.data.api.model.news
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Source(val name: String? = null, val id: Any? = null) : Parcelable {
-    constructor(parcel: Parcel) : this(parcel.readString(), parcel.readString())
+data class Source(val id: String? = null,
+                  val name: String? = null,
+                  val description: String? = null,
+                  val url: String? = null,
+                  val category: String? = null,
+                  val language: String? = null,
+                  val country: String? = null) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString())
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
         parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeString(url)
+        parcel.writeString(category)
+        parcel.writeString(language)
+        parcel.writeString(country)
     }
 
     override fun describeContents(): Int {
@@ -23,4 +42,5 @@ data class Source(val name: String? = null, val id: Any? = null) : Parcelable {
             return arrayOfNulls(size)
         }
     }
+
 }
