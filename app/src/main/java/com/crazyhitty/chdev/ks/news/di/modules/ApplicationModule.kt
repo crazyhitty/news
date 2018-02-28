a1646199a6900ff5db7e7cf0dc57e9e25aff6966
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-02-28 16:13:56 +0530

    Reduce Okhttp request timeout to 15 seconds

--- app/src/main/java/com/crazyhitty/chdev/ks/news/di/modules/ApplicationModule.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/di/modules/ApplicationModule.kt
@@ -100,9 +100,9 @@ class ApplicationModule(private val application: Application) {
                                 .build())
                     }
                     .addNetworkInterceptor(networkInterceptor)
-                    .connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .readTimeout(15, TimeUnit.SECONDS)
+                    .writeTimeout(15, TimeUnit.SECONDS)
                     .build()
 
     @Provides
