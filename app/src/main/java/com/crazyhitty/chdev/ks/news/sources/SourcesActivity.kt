cc2273da89dad528e6bc1c26faf912943832d9e3
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-02-28 11:05:36 +0530

    [WIP] Sources UI

---
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/sources/SourcesActivity.kt
@@ -0,0 +1,13 @@
+package com.crazyhitty.chdev.ks.news.sources
+
+import android.os.Bundle
+import com.crazyhitty.chdev.ks.news.base.BaseAppCompatActivity
+
+/**
+ * @author  Kartik Sharma (cr42yh17m4n@gmail.com)
+ */
+class SourcesActivity: BaseAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
