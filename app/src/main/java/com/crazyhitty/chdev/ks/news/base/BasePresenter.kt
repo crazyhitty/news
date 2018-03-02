1e341ab9337045947d06ac9567b2f64812e1fc44
Kartik Sharma <cr42yh17m4n@gmail.com>
2018-03-02 22:35:16 +0530

    Add documentation for some classes

--- app/src/main/java/com/crazyhitty/chdev/ks/news/base/BasePresenter.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/base/BasePresenter.kt
@@ -1,10 +1,21 @@
 package com.crazyhitty.chdev.ks.news.base
 
 /**
+ * Every presenter should implement this presenter.
+ *
  * @author  Kartik Sharma (cr42yh17m4n@gmail.com)
  */
 interface BasePresenter<in V : BaseView> {
+    /**
+     * Called when a view is attached with this presenter.
+     *
+     * @param view  The view with which this presenter would interact with.
+     */
     fun onAttach(view: V)
 
+    /**
+     * Called when a view is detached from this presenter. You should also release any resources
+     * held by this presenter in this method's body.
+     */
     fun onDetach()
 }
--- app/src/main/java/com/crazyhitty/chdev/ks/news/base/BaseView.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/base/BaseView.kt
@@ -1,6 +1,8 @@
 package com.crazyhitty.chdev.ks.news.base
 
 /**
+ * Every View must implement this interface.
+ *
  * @author  Kartik Sharma (cr42yh17m4n@gmail.com)
  */
 interface BaseView {
