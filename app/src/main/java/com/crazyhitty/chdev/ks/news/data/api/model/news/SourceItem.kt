a3516fec46f894004709c74a92b7721b201efca8
Kartik Sharma <kartik.sharma@myntra.com>
2018-04-09 00:18:50 +0530

    Add spannable name in SourceItem

--- app/src/main/java/com/crazyhitty/chdev/ks/news/data/api/model/news/SourceItem.kt
+++ app/src/main/java/com/crazyhitty/chdev/ks/news/data/api/model/news/SourceItem.kt
@@ -1,7 +1,6 @@
 package com.crazyhitty.chdev.ks.news.data.api.model.news
 
-import android.os.Parcel
-import android.os.Parcelable
+import android.text.Spannable
 
 data class SourceItem(val id: String? = null,
                       val name: String? = null,
@@ -10,39 +9,5 @@ data class SourceItem(val id: String? = null,
                       val category: String? = null,
                       val language: String? = null,
                       val country: String? = null,
-                      var selected: Boolean? = null) : Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readValue(Boolean::class.java.classLoader) as? Boolean)
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(name)
-        parcel.writeString(description)
-        parcel.writeString(url)
-        parcel.writeString(category)
-        parcel.writeString(language)
-        parcel.writeString(country)
-        parcel.writeValue(selected)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<SourceItem> {
-        override fun createFromParcel(parcel: Parcel): SourceItem {
-            return SourceItem(parcel)
-        }
-
-        override fun newArray(size: Int): Array<SourceItem?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+                      var selected: Boolean? = null,
+                      @Transient var spannableName: Spannable? = null)
